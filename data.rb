ca5bbc2cc3a23f49a25a3b6c1e957f937ac0ce95
Miyuki Koshiba <ksbmyk@gmail.com>
2025-07-21 22:10:34 +0900

    change character design

--- data.rb
+++ data.rb
@@ -1,31 +1,34 @@
 # Fill in the blank with your name
-$author = "" # Your name
+$author = "chobishiba" # Your name
 
 # You can modify and add colors here
 $palette = {
   'w' => 231,
   'p' => 217,
-  'b' => 232,
+  'b' => 248,
   'g' => 34,
   'y' => 178,
   'd' => 22,
   'r' => 196,
+  'v' => 124,
+  'k' => 0,
 }
 
 # Rapiko's height should be <= 15
 # Rapiko's fur color should be white
 $rapiko = %w[
-  ......www..ww
-  ......wpw..wp
-  ....wwwpwwwwpw
-  ...wwwwwwwwwwww
-  ...wwwwwbwwwwbw
-  ....wwwwwwwbwww
-  .....wwwwwwwww
-  .....pppppppppp
-  ....ppwwppppppww
-  ...wpppppppppp
-  .....www....www
+  ....wwwwww
+  ......wwwwww
+  ........wwwwww
+  wwwwwwwwwwwwwwww
+  wwppppppwwwwvvwwww
+  ........wwwwwwwwwwpp
+  ........wwwwwwwwww
+  ....wwwwwwwwwwww
+  ..wwwwwwwwwwwwww
+  wwwwwwbbbbwwwwww
+  wwwwwwwwwwbbwwwwww
+  ....wwwwwwwwbbwwwwww
 ]
 
 # Camerlengo's height should be equal to Rapiko
@@ -34,23 +37,27 @@ $camerlengo = %w[
   .
   .
   .
-  .............yyyyy
-  ............yyyddyy
-  ....ggggggggyyyyyyy
-  ...ggggggggggyyyyy
-  ..gggggggggggg
-  yygggggggggggg
-  ...yyy......yyy
+  ........gggddddggg
+  ......gddddggggddddg..yyyy
+  ....dddggggddddggggdddyykkyy
+  ....gggddddggggddddgggyyyyyy
+  yy..dddggggddddggggdddyyyy
+  ..yyddddddddddddddddddyy
+  ....yyyy..........yyyy
+  ....yy..............yy
+
 ]
 
 # Bullet should be Ruby
 $bullet = %w[
-  .
-  .
-  ......rrrrrr
-  ....rrrrrrrrrr
-  ..rrrrrrrrrrrrrr..
-  ....rrrrrrrrrr
-  ......rrrrrr
-  ........rr
+  ....vvvvwwrrrrrrvvvvvv
+  ..vvvvwwrrrrrrrrrrvvvvvv
+  vvvvvvvvrrrrrrrrrrvvvvvvvv
+  vvwwwwrrwwrrwwwwwwrrwwwwvv
+  ..vvvvrrrrrrrrrrrrrrvvvv
+  ....vvvvrrrrrrrrrrvvvv
+  ......vvrrrrrrrrrrvv
+  ........vvrrrrrrvv
+  ..........rrrrrr
+  ............rr
 ]
