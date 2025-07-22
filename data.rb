e88c65c7604c3cd84101648f8c0745467d2a1bdb
Miyuki Koshiba <ksbmyk@gmail.com>
2025-07-22 22:07:20 +0900

    adjust to 5px in height

--- data.rb
+++ data.rb
@@ -1,11 +1,9 @@
 # Fill in the blank with your name
-$author = "" # Your name
+$author = "chobishiba" # Your name
 
 # You can modify and add colors here
 $palette = {
   'w' => 231,
-  'p' => 217,
-  'b' => 232,
   'g' => 34,
   'y' => 178,
   'd' => 22,
@@ -15,42 +13,26 @@ $palette = {
 # Rapiko's height should be <= 15
 # Rapiko's fur color should be white
 $rapiko = %w[
-  ......www..ww
-  ......wpw..wp
-  ....wwwpwwwwpw
-  ...wwwwwwwwwwww
-  ...wwwwwbwwwwbw
-  ....wwwwwwwbwww
-  .....wwwwwwwww
-  .....pppppppppp
-  ....ppwwppppppww
-  ...wpppppppppp
-  .....www....www
+  .w.w
+  .w.w
+  wrwrw
+  wwwww
+  .www
 ]
 
 # Camerlengo's height should be equal to Rapiko
 $camerlengo = %w[
-  .
-  .
-  .
-  .
-  .............yyyyy
-  ............yyyddyy
-  ....ggggggggyyyyyyy
-  ...ggggggggggyyyyy
-  ..gggggggggggg
-  yygggggggggggg
-  ...yyy......yyy
+    .
+  ...yd
+  .ggyy
+  gggg
+  y..y
 ]
 
 # Bullet should be Ruby
 $bullet = %w[
-  .
-  .
-  ......rrrrrr
-  ....rrrrrrrrrr
-  ..rrrrrrrrrrrrrr..
-  ....rrrrrrrrrr
-  ......rrrrrr
-  ........rr
+  .rrr
+  rrrrr
+  .rrr
+  ..r
 ]
