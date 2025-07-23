be4f9a5f45b99efcb8e2d9304d105c3187d35ad9
youchan <youchan01@gmail.com>
2025-07-23 09:11:41 +0900

    Youchan's submitted artwork

--- data.rb
+++ data.rb
@@ -1,13 +1,16 @@
 # Fill in the blank with your name
-$author = "" # Your name
+$author = "youchan" # Your name
 
 # You can modify and add colors here
 $palette = {
   'w' => 231,
   'p' => 217,
+  's' => 160,
+  'l' => 224,
   'b' => 232,
   'g' => 34,
-  'y' => 178,
+  'o' => 239,
+  'y' => 220,
   'd' => 22,
   'r' => 196,
 }
@@ -15,31 +18,29 @@ $palette = {
 # Rapiko's height should be <= 15
 # Rapiko's fur color should be white
 $rapiko = %w[
-  ......www..ww
-  ......wpw..wp
-  ....wwwpwwwwpw
-  ...wwwwwwwwwwww
-  ...wwwwwbwwwwbw
-  ....wwwwwwwbwww
-  .....wwwwwwwww
-  .....pppppppppp
-  ....ppwwppppppww
-  ...wpppppppppp
-  .....www....www
+  ...www..www
+  ....wpw..wpw
+  ....wpw..wpw
+  ....wwwwwwwww
+  ...wwwwwbwwbww
+  ....wwwwwwwrrrs
+  .....wwwwwrlrrrs
+    ....pppppwwwrsww
+  ..wwpppppppp
+  ...www....www
 ]
 
 # Camerlengo's height should be equal to Rapiko
 $camerlengo = %w[
   .
-  .
-  .
-  .
-  .............yyyyy
-  ............yyyddyy
-  ....ggggggggyyyyyyy
-  ...ggggggggggyyyyy
-  ..gggggggggggg
-  yygggggggggggg
+  .....rrrs.....yyyy
+  ....rlrrrs...yyydyy
+  ......rs.....yyyyyy
+  ....gogogog..yyyyy
+  ...gooooooogyyyyy
+  ..gggogogogggyy
+  .gggoooooooggg
+  yygggogogogggg
   ...yyy......yyy
 ]
 
@@ -47,10 +48,11 @@ $camerlengo = %w[
 $bullet = %w[
   .
   .
-  ......rrrrrr
-  ....rrrrrrrrrr
-  ..rrrrrrrrrrrrrr..
-  ....rrrrrrrrrr
-  ......rrrrrr
-  ........rr
+  .....llrrrrrs
+  ...llrrrrrrrsss
+  ..rllrrrssssssss..
+  ...rrrrrrrrssss
+  .....rrrrrsss
+  .......rrss
+  ........rs
 ]
